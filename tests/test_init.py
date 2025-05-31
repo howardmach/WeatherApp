1498e844f6eea5c7c3d1b07d949334711330802a
howardmach <howardmach23@hotmail.com>
2025-05-31 17:05:23 -0500

    Delete tests directory

--- tests/test_init.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-def test_create_app():
-    app = create_app()
-    assert app is not None
-    assert app.name == "app"
-    # Optionally, check if blueprints are registered
-    assert "main" in app.blueprints
